4fa2f973619196bbeae12e45c157ef12faff930b
James McDuffie <James.McDuffie@jpl.nasa.gov>
2023-01-31 09:50:46 -0800

    Add skeleton version of Unity script for processing repos using app-pack-generator.

---
+++ process_repo.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from app_pack_generator.parser import GitHelper, DockerUtil, AppNB, Util
+
+def main(args):
+
+    # Performs the following steps:
+    # * Pushes a copy of source Git repo into Unity Gitlab for storage
+    # * Builds a Docker image using repo2docker 
+    # * Pushes Docker image into a container registry
+    # * Creates CWL files from application metadata and Docker registry URL
+    # * Register application with Dockstore
+    # * Submits CWL files to Dockstore
+
+    source_repo = args[1]
+    build_dir = args[2]
+
+    repo_dir = os.path.realpath(os.path.join(build_dir, "application_repo"))
+    cwl_dir = os.path.realpath(os.path.join(build_dir, "cwl"))
+
+    # Check out original repository
+    repo = GitHelper(source_repo, dst=repo_dir)
+    #repo.Checkout(checkout)
+    os.chdir(repo_dir)
+
+    # Create Docker image
+    docker_util = DockerUtil(repo, do_prune=False)
+    image_tag = docker_util.Repo2Docker()
+    dockerurl = image_tag
+    #dockerurl = docker_util.PushImage(image_tag, docker_registry)
+
+    # Generate CWL artifacts within the output directory.
+    nb = AppNB(repo)
+    files = nb.Generate(cwl_dir, dockerurl)
+
+    return_code = 0
+
+    return return_code
+
+if __name__ == '__main__':
+    time_ms, ret = Util.TimeFunction(main, sys.argv)
+    print('Execution Time:', time_ms, 'ms (returned', str(ret) + ')')
+    exit(ret)
